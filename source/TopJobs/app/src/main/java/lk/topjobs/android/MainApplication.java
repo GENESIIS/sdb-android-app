2db2e16d45d74f35889ad35e302be927c66e2b5f
pasindus <pasindu@genesiis.com>
2018-04-17 14:02:08 +0530

    v1.1 Added gradle converted source code

---
+++ source/TopJobs/app/src/main/java/lk/topjobs/android/MainApplication.java
@@ -0,0 +1,71 @@
+package lk.topjobs.android;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import lk.topjobs.android.data.JobCategoryData;
+import lk.topjobs.android.data.JobPostData;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.Application;
+import android.content.res.AssetManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @author Harsha Kodagoda
+ *
+ * 20180411 SDB-668-4472 PS softcoded host to change when needed and made the host varibale as a public static to access from aother classes
+ */
+public class MainApplication extends Application {
+	private ArrayList<JobCategoryData> jobCategoryList;
+	public ArrayList<JobPostData> originalJobList;
+	public ArrayList<JobPostData> filteredJobList;
+	public ArrayList<JobPostData> favoriteJobList;
+	public static String host = "http://100.100.100.198/";
+
+	public ArrayList<JobCategoryData> getJobCategoryList() {
+		if (jobCategoryList == null) {
+			jobCategoryList = loadRSSFeedList();
+		}
+		return jobCategoryList;
+	}
+
+	private ArrayList<JobCategoryData> loadRSSFeedList() {
+		AssetManager assetManager = getApplicationContext().getResources()
+				.getAssets();
+		InputStream inputStream = null;
+		try {
+			inputStream = assetManager.open("categories.json");
+			if (inputStream != null) {
+				StringBuilder stringBuilder = new StringBuilder();
+				BufferedReader bufferedReader = new BufferedReader(
+						new InputStreamReader(inputStream));
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+				ArrayList<JobCategoryData> categoryList = new ArrayList<JobCategoryData>();
+				JSONArray jsonArray = new JSONArray(stringBuilder.toString());
+				for (int i = 0; i < jsonArray.length(); i++) {
+					JSONObject jsonObject = jsonArray.getJSONObject(i);
+					JobCategoryData jobCategoryData = new JobCategoryData();
+					jobCategoryData.setId(jsonObject.getInt("id"));
+					jobCategoryData.setName(jsonObject.getString("name"));
+					jobCategoryData.setUrl(host + jsonObject.getString("url")); // SDB-668-4472 Append host in to the url getting from the json
+					categoryList.add(jobCategoryData);
+				}
+				return categoryList;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
---
+++ source/TopJobs/app/src/main/java/lk/topjobs/android/data/JobPostData.java
@@ -0,0 +1,164 @@
+package lk.topjobs.android.data;
+
+import java.util.Date;
+
+import lk.topjobs.android.MainApplication;
+
+/**
+ * @author Harsha Kodagoda
+ *
+ * 20180411 SDB-668-4472-PS Append the host into the hardcoded advertisement Url to get the full url from the MainApplication
+ * 
+ */
+public class JobPostData {
+	private static String advertisementUrl = MainApplication.host + "employer/advertismentpreview.jsp";
+	private int id;
+	private String title;
+	private String position;
+	private String employer;
+	private String link;
+	private String description;
+	private Date pubDate;
+	private Date closingDate;
+	private String ac;
+	private String js;
+	private String ec;
+
+	// private boolean favorite = false;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title.replace("�", "");
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPosition() {
+		return position.replace("�", "");
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getEmployer() {
+		return employer.replace("�", "");
+	}
+
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getDescription() {
+		return description.replace("�", "");
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public Date getClosingDate() {
+		return closingDate;
+	}
+
+	public void setClosingDate(Date closingDate) {
+		this.closingDate = closingDate;
+	}
+
+	public String getAc() {
+		return ac;
+	}
+
+	public void setAc(String ac) {
+		this.ac = ac;
+	}
+
+	public String getJs() {
+		return js;
+	}
+
+	public void setJs(String js) {
+		this.js = js;
+	}
+
+	public String getEc() {
+		return ec;
+	}
+
+	public void setEc(String ec) {
+		this.ec = ec;
+	}
+
+	// public boolean isFavorite() {
+	// return favorite;
+	// }
+	//
+	// public void setFavorite(boolean favorite) {
+	// this.favorite = favorite;
+	// }
+
+	public void splitTitle() {
+		try {
+			if (getJs().length() > 0 && getTitle().length() > 0) {
+				String[] titleParts = getTitle().split(getJs());
+				if (titleParts.length >= 2) {
+					String position = titleParts[0].trim();
+					if (position.endsWith(" -")) {
+						position = position.substring(0, position.length() - 2);
+					}
+					setPosition(position.replace("�", ""));
+					String employer = titleParts[1].trim();
+					if (employer.startsWith("- ")) {
+						employer = employer.substring(2);
+					}
+					setEmployer(employer.replace("�", ""));
+				}
+			}
+		} catch (Exception ex) {
+		}
+	}
+
+	public String getRefNo() {
+		return getJs();
+	}
+
+	public String getAdvertisementLink() {
+		try {
+			if (getAc().length() > 0 && getJs().length() > 0
+					&& getEc().length() > 0) {
+				String params = "?jc=" + getJs() + "&ac=" + getAc() + "&ec="
+						+ getEc();
+				return advertisementUrl + params;
+			} else {
+				return "";
+			}
+		} catch (Exception ex) {
+			return "";
+		}
+	}
+}
